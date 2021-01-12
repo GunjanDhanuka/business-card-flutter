c577a43394601d474116ffa59bfbba4539e17b6c
Gunjan Dhanuka <lucifergd192@gmail.com>
2021-01-12 13:25:46 +0530

    Added screenshot of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,110 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage(
+                  'images/gunjan.jpg',
+                ),
+              ),
+              Text(
+                'Gunjan Dhanuka',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.teal.shade100,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    '+91-72402-27672',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                color: Colors.teal.shade100,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.red,
+                  ), // Icon
+                  title: Text(
+                    'd.gunjan@iitg.ac.in',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// children: [
+// Icon(
+// Icons.email,
+// color: Colors.teal,
+// ),
+// SizedBox(
+// width: 10.00,
+// ),
+// Text(
+// 'd.gunjan@iitg.ac.in',
+// style: TextStyle(
+// fontFamily: 'Source Sans Pro',
+// color: Colors.teal.shade900,
+// fontSize: 20.0,
+// ),
+// )
+// ],
+// ),
